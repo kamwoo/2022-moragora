cd4a6cd0a7534e7be5f904ae6cd3d443bfaca541
kamwoo <rkadndud06@naver.com>
2022-08-17 02:46:23 +0900

    style: MeetingPage에서 CoffeeStackPage로 네이밍 수정

--- frontend/src/mocks/handlers/index.ts
+++ frontend/src/mocks/handlers/index.ts
@@ -2,10 +2,11 @@ import commonHandler from './commonHandler';
 import meetingHandler from './meetingHandler';
 import eventHandler from './eventHandler';
 import userHandler from './userHandler';
+import meetingHandler_new from './meetingHandler_new';
 
 const handlers = [
   ...commonHandler,
-  ...meetingHandler,
+  ...meetingHandler_new,
   ...eventHandler,
   ...userHandler,
 ];
